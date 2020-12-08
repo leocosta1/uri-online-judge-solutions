2e31623f66260b5a38960184cfb9ce0cf2c3e2d5
Leonardo Costa <leorizzocosta@hotmail.com>
2020-12-08 11:39:46 -0300

    Solution of problem 1020

---
+++ 1-begginer/cpp/1020-age-in-days/1020.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    int a[3] = {365, 30, 1};
+    int age[3] = {0};
+    cin >> n;
+
+    for(int i=0; i<3; i++) {
+        age[i] = n / a[i];
+        n -= age[i] * a[i];
+    }
+
+    printf("%i ano(s)\n%i mes(es)\n%i dia(s)\n", age[0], age[1], age[2]);
+
+    return 0;
+}
