37afc1d326aeafb434138d86848a041e44bbe21b
Leonardo Costa <leorizzocosta@hotmail.com>
2020-12-07 22:40:44 -0300

    Solution of problem 1008

---
+++ 1-begginer/cpp/1008-salary/1008.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number, workTime;
+    double moneyHour;
+
+    cin >> number;
+    cin >> workTime;
+    cin >> moneyHour;
+
+    double salary = workTime * moneyHour;
+
+    printf("NUMBER = %i\n", number);
+    printf("SALARY = U$ %.2lf\n", salary);
+
+    return 0;
+}
