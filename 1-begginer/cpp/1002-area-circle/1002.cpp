50fb15924c1756d30538326433297f82a49bb9ec
Leonardo Costa <leorizzocosta@hotmail.com>
2020-12-07 22:39:46 -0300

    Solution of problem 1002

---
+++ 1-begginer/cpp/1002-area-circle/1002.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double r, pi = 3.14159;
+    cin >> r;
+
+    double a = pi * (r * r);
+
+    printf("A=%.4lf\n", a);
+
+    return 0;
+}
