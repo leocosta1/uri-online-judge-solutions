811ea7eb7fb85f41205495ce8bc8d0e2421b5751
Leonardo Costa <leorizzocosta@hotmail.com>
2020-12-07 22:40:23 -0300

    Solution of problem 1004

---
+++ 1-begginer/cpp/1004-simple-product/1004.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a;
+    cin >> b;
+
+    int prod = a * b;
+
+    cout << "PROD = " << prod << endl;
+
+    return 0;
+}
