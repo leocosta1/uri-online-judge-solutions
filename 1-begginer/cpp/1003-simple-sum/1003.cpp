0ac1ba752bb177956107314abb33b39e06280e5f
Leonardo Costa <leorizzocosta@hotmail.com>
2020-12-07 22:39:53 -0300

    Solution of problem 1003

---
+++ 1-begginer/cpp/1003-simple-sum/1003.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a;
+    cin >> b;
+
+    int soma = a + b;
+
+    cout << "SOMA = " << soma << endl;
+
+    return 0;
+}
