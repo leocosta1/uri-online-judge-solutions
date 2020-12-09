08d41457a673f1ba45dfbb1eae6cb546ea4300d0
Leonardo Costa <leorizzocosta@hotmail.com>
2020-12-09 10:16:00 -0300

    Solution of problem 1049

---
+++ 1-begginer/cpp/1049-animal/1049.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string a, b, c;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+
+    if(a == "vertebrado") {
+        if(b == "ave") {
+            if(c == "carnivoro") {
+                cout << "aguia\n";
+            }
+            else if(c == "onivoro") {
+                cout << "pomba\n";
+            }
+        }
+        else if(b == "mamifero") {
+            if(c == "onivoro") {
+                cout << "homem\n";
+            }
+            else if(c == "herbivoro") {
+                cout << "vaca\n";
+            }
+        }
+    }
+    else if(a == "invertebrado") {
+        if(b == "inseto") {
+            if(c == "hematofago") {
+                cout << "pulga\n";
+            }
+            else if(c == "herbivoro") {
+                cout << "lagarta\n";
+            }
+        }
+        else if(b == "anelideo") {
+            if(c == "hematofago") {
+                cout << "sanguessuga\n";
+            }
+            else if(c == "onivoro") {
+                cout << "minhoca\n";
+            }
+        }
+    }
+
+    return 0;
+}
