ec1ae306ac5d3e6320848273845de030c8f526b6
Leonardo Costa <leorizzocosta@hotmail.com>
2020-12-09 10:17:10 -0300

    Solution of problem 1071

---
+++ 1-begginer/cpp/1071-sum-consecutive-odd1/1071.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x, y, sum = 0;
+    cin >> x;
+    cin >> y;
+
+    for(int i = x - 1; i > y; i--) {
+        if(i % 2 != 0) {
+            sum += i;
+        }
+    }
+
+    cout << sum << endl;
+
+    return 0;
+}
