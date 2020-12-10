0fcd796c904a3a8d8691fe88c151df7b3aae137a
Leonardo Costa <leorizzocosta@hotmail.com>
2020-12-10 15:10:03 -0300

    Solution of problem 1115

---
+++ 1-begginer/cpp/1115-quadrant/1115.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x, y;
+    cin >> x >> y;
+
+    while(x != 0 && y != 0) {
+
+        if(x > 0 && y > 0) {
+            cout << "primeiro\n";
+        }
+        else if(x < 0 && y > 0) {
+            cout << "segundo\n";
+        }
+        else if(x < 0 && y < 0) {
+            cout << "terceiro\n";
+        }
+        else {
+            cout << "quarto\n";
+        }
+
+        cin >> x >> y;
+    }
+
+    return 0;
+}
