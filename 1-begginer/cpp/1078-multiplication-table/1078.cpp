9cff31c6f80a365997227b91d8b68433524740a0
Leonardo Costa <leorizzocosta@hotmail.com>
2020-12-10 11:34:56 -0300

    Solution of problem 1078

---
+++ 1-begginer/cpp/1078-multiplication-table/1078.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for(int i = 1; i <= 10; i++) {
+        printf("%d x %d = %d\n", i, n, (i * n));
+    }
+
+    return 0;
+}
