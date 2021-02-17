47392f57b90b21defa0923673d855e364994057e
Leonardo Costa <leorizzocosta@hotmail.com>
2021-02-17 17:28:56 -0300

    Solution of problem 1190

---
+++ 1-begginer/cpp/1190-right-area/1190.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char o;
+    cin >> o;
+
+    double m[12][12];
+    for(int i = 0; i < 12; i++) {
+        for(int j = 0; j < 12; j++) {
+            cin >> m[i][j];
+        }
+    }
+
+    double total = 0, cont = 0;
+    for(int i = 0; i < 12; i++) {
+        for(int j = 0; j < 12; j++) {
+            if((i < j) && (j > (11 - i))) {
+                total += m[i][j];
+                cont++;
+            }
+        }
+    }
+
+    if(toupper(o) == 'M') {
+        total = total / cont;
+    }
+
+    printf("%.1lf\n", total);
+
+    return 0;
+}
