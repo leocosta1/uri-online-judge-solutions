ae7e9781b6bd79234cbf48d4edc08d81fb6aad00
Leonardo Costa <leorizzocosta@hotmail.com>
2021-02-21 15:11:29 -0300

    Solution of problem 1564

---
+++ 1-begginer/cpp/1564-brazil-world-cup/1564.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    while(cin >> n) {
+        if(n == 0) {
+            printf("vai ter copa!\n");
+        }
+        else {
+            printf("vai ter duas!\n");
+        }
+    }
+
+    return 0;
+}
