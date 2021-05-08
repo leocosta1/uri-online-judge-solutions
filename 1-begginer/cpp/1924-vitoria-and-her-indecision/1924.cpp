580073112bf90bda7700251d0b2dfd34bb5e4eda
Leonardo Costa <leorizzocosta@hotmail.com>
2021-05-07 23:06:36 -0300

    Solution of problem 1924

---
+++ 1-begginer/cpp/1924-vitoria-and-her-indecision/1924.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for(int i = 0; i < n; i++) {
+        string s;
+        cin >> s;
+    }
+
+    printf("Ciencia da Computacao\n");
+
+    return 0;
+}
